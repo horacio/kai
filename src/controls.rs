c5572ffe205819b7897e262c78a67f6e04754e6d
cy6erlion <cy6erlion@protonmail.com>
2019-09-04 16:19:37 +0200

    moved to controls to controls module

--- src/controls.rs
+++ src/controls.rs
@@ -1,3 +1,8 @@
+use crate::todo::Todo;
+use std::io;
+
+use console::style;
+
 pub struct Ctrl {
     pub stage: String,
     pub cmd: String,
@@ -14,4 +19,31 @@ impl Ctrl {
 
         Ok(Ctrl { stage, cmd })
     }
+
+    pub fn process_todo(mut todo: Todo) {
+        let mut user_input = String::new();
+        loop {
+            io::stdin()
+                .read_line(&mut user_input)
+                .expect("Failed to read line");
+
+            match user_input.as_ref() {
+                "\n" | "q\n" | "Q\n" => {
+                    println!("{}", style("Quit").red());
+                    break;
+                }
+                "s\n" | "S\n" => {
+                    todo.create_todo_file();
+                    break;
+                }
+                _ => {
+                    todo.tasks.push(user_input.trim().to_string());
+                }
+            }
+
+            user_input = String::new();
+        }
+    }
+    
+    pub fn process_pomo() {}
 }
