ce9b0f88984632adc06a05a15c4c57cb69418541
cy6erlion <cy6erlion@protonmail.com>
2019-09-04 12:47:25 +0200

    pretiffied clock output

--- src/clock.rs
+++ src/clock.rs
@@ -1,15 +1,25 @@
 use std::thread;
 use std::time::Duration;
 
+use console::Term;
+use console::style;
+
 pub fn countdown(count_from: i32) -> Result<(), ()>{
+    let term = Term::stdout();
+    
     let handle = thread::spawn(move || {
-        println!("[⏰ COUNTDOWN STARTED]");
-                 
+        
         for i in (1..=count_from).rev() {
-            println!("{}min remaining...", i);
+            term.clear_screen().unwrap();
+            println!("{}", style("FOCUS").yellow());        
+            println!("{}", style("---------------------").yellow());
+            println!("{}", style("[COUNTDOWN STARTED]").red());
+            
+            println!("{}", style(format!("⏰ {}min remaining...", i)).blue());
             thread::sleep(Duration::from_millis(60000));
         }
-        println!("[⏰ COUNTDON ENDED]");
+        println!("{}", style("[COUNTDOWN ENDED]").green());
+        println!("{}", style("---------------------").yellow());
     });
 
     handle.join().unwrap();
