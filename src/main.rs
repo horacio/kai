6f697637939f0e3d4576b11f903903b389d0d742
cy6erlion <cy6erlion@protonmail.com>
2019-09-04 10:34:52 +0200

    made main to work with App struct

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ use iwah::controls::Ctrl;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let cmd = Ctrl::new(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(e) = iwah::run(cmd) {
+    let app = iwah::App{};
+
+    if let Err(e) = app.run(cmd) {
         eprintln!("Application error {}", e);
         process::exit(1);
     }
