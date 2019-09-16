ad352709e42070ca41e74f08f6d33e7056779ecf
cy6erlion <cy6erlion@protonmail.com>
2019-09-16 15:11:30 +0200

    removed date-time crate, using custom date code

---
+++ src/date.rs
@@ -0,0 +1,23 @@
+use std::process::{Command, Stdio};
+
+#[derive(Debug)]
+pub struct Date {
+    pub date: String,
+}
+
+impl Date {
+    pub fn today() -> String {
+        let getdate = Command::new("/bin/date")
+            .arg("-I")
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("failed to execute child");
+
+        let output = getdate.wait_with_output().expect("failed to wait on child");
+
+        let date = String::from_utf8(output.stdout).expect("Error getting todays date");
+        let date = String::from(date.trim());
+
+        date
+    }
+}
