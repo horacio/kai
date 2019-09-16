abccfa4afc30922a6533cb9fe256a3fe956a6b0a
cy6erlion <cy6erlion@protonmail.com>
2019-09-16 15:15:32 +0200

    removed date-time crate and added custom date code

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use std::error::Error;
-extern crate chrono;
 
 pub mod clock;
+pub mod date;
 pub mod controls;
 pub mod todo;
 
-use chrono::Local;
 use clock::countdown;
+use date::Date;
 
 const POMO_DEFAULT_TIME: i32 = 25;
 
@@ -15,9 +15,9 @@ pub struct App {}
 impl App {
     pub fn run(&self, input: controls::Ctrl) -> Result<(), Box<dyn Error>> {
         match (input.stage.as_ref(), input.cmd.as_ref()) {
-            ("todo", "today") => todo::Todo::new(&Local::today().to_string()),
+            ("todo", "today") => todo::Todo::new(&Date::today()),
             ("todo", title) => todo::Todo::new(title),
-            ("clock", "today") => countdown(POMO_DEFAULT_TIME).unwrap(),
+            ("clock", "today") => countdown(POMO_DEFAULT_TIME, &Date::today()).unwrap(),
             (&_, &_) => (),
         }
 
