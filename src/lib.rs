7ef35763636a1bed518845ceea56a99dbc8f78fd
cy6erlion <cy6erlion@protonmail.com>
2019-10-15 14:23:26 +0200

    cleaned module importing

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,6 @@ pub mod controls;
 pub mod date;
 pub mod todo;
 
-use clock::countdown;
-use date::Date;
-
 const POMO_DEFAULT_TIME: i32 = 25;
 
 pub struct App {}
@@ -15,9 +12,9 @@ pub struct App {}
 impl App {
     pub fn run(&self, input: controls::Ctrl) -> Result<(), Box<dyn Error>> {
         match (input.stage.as_ref(), input.cmd.as_ref()) {
-            ("todo", "today") => todo::Todo::new(&Date::today()),
+            ("todo", "today") => todo::Todo::new(&date::Date::today()),
             ("todo", title) => todo::Todo::new(title),
-            ("clock", "today") => countdown(POMO_DEFAULT_TIME, &Date::today()).unwrap(),
+            ("clock", "today") => clock::countdown(POMO_DEFAULT_TIME, &date::Date::today()).unwrap(),
             (&_, &_) => (),
         }
 
