bcdae4f4331bbd11c4ae947f0a12520c890587f5
cy6erlion <cy6erlion@protonmail.com>
2019-12-30 16:14:00 +0200

    added clap for parsing cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,137 @@
-use std::env;
-use std::process;
+extern crate clap;
+extern crate dirs;
 
-use kai::controls::Ctrl;
+use clap::{Arg, App, SubCommand};
+use std::error::Error;
+use std::fs;
+
+pub mod clock;
+pub mod controls;
+pub mod date;
+pub mod db;
+pub mod errors;
+pub mod log;
+pub mod todo;
+
+const KAI_DEFAULT_TIME: u64 = 1; // 1 KAI = 25 minutes
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let cmd = Ctrl::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+    let commands = App::new("kai")
+        .version("1.0")
+        .author("cy6erlion <cy6erlion@protonmail.com>")
+        .about("Time management system.")
+        .arg(Arg::with_name("config")
+             .short("c")
+             .long("config")
+             .value_name("FILE")
+             .help("Sets a custom config file")
+             .takes_value(true))
+        .subcommand(SubCommand::with_name("todo")
+                    .name("todo")
+                    .about("Create a new todo")
+                    .arg(Arg::with_name("source")
+                         .short("s")
+                         .long("source")
+                         .takes_value(true)
+                         .value_name("SOURCE")
+                         .default_value("today")
+                         .number_of_values(1)))
+        .subcommand(SubCommand::with_name("clock")
+                    .name("clock")
+                    .about("Countdown timer (defaults to 1 KAI (25min))")
+                    .arg(Arg::with_name("todo")
+                         .short("t")
+                         .long("todo")
+                         .takes_value(true)
+                         .value_name("TODO")
+                         .default_value("today")
+                         .number_of_values(1)))
+        .subcommand(SubCommand::with_name("log")
+                    .name("log")
+                    .about("Analyse and track your work habbits")
+                    .arg(Arg::with_name("option")
+                         .short("o")
+                         .long("option")
+                         .takes_value(true)
+                         .value_name("OPTION")
+                         .default_value("all")
+                         .number_of_values(1)))
+        .get_matches();
+    
+    match commands.subcommand() {
+        ("todo",  Some(todo)) => {
+            match todo.value_of("source") {
+                Some("today") => todo::Todo::new(&date::Date::today()),
+                Some(title) => {
+                    if title.ends_with(".md") {
+                        todo::parse(title);
+                    } else {
+                        todo::Todo::new(title)
+                    }                
+                }
+                _ => {},
+            }
+        }
+        
+        ("clock", Some(clock)) => {
+            match clock.value_of("todo") {
+                Some("today") => {
+                    if let Err(e) = detect_todo(&date::Date::today()) {
+                        println!("{}", e);
+                    } else {
+                        clock::countdown(KAI_DEFAULT_TIME, &date::Date::today()).unwrap();
+                    }
+                },
+                Some(title) => {
+                    if let Err(e) = detect_todo(title) {
+                        println!("{}", e);
+                    } else {
+                        clock::countdown(KAI_DEFAULT_TIME, title).unwrap();
+                    }
+                },
+                _ => {},
+            }
+        },
 
-    let app = kai::App {};
-
-    if let Err(e) = app.run(cmd) {
-        eprintln!("Application error {}", e);
-        process::exit(1);
+        ("log", Some(log)) => {
+            match log.value_of("option") {
+                Some("all") => {
+                    log::all();
+                },
+                Some("today") => {
+                    if let Err(e) = detect_todo(&date::Date::today()) {
+                        println!("{}", e);
+                    } else {
+                        log::basic(&date::Date::today());
+                    }
+                },
+                Some(title) => {
+                    if let Err(e) = detect_todo(title) {
+                        println!("{}", e);
+                    } else {
+                        log::basic(title);
+                    }
+                },
+                _ => {},
+            }
+        },
+        _ => {},
     }
+}
+
+// check if todo file exists
+fn detect_todo(filename: &str) -> Result<(), Box<dyn Error>> {
+    let kai_store = format!("{}/.kai", dirs::home_dir().unwrap().display());
+    let entries = fs::read_dir(kai_store).unwrap();
+    let file = format!("{}.json", filename);
+
+    for entry in entries {
+        if file == entry.unwrap().file_name().to_str().unwrap() {
+            return Ok(());
+        }
+    }
+
+    return Result::Err(Box::new(errors::TodoErr(
+        "Todo entry does not exist".into(),
+    )));
 }
--- src/todo/parser.rs
+++ src/todo/parser.rs
@@ -1,4 +1,6 @@
-pub fn parse() {
+use std::fs;
 
+pub fn parse(filename: &str){
+    let file = fs::read(filename).expect("file not found");
+    println!("{:?}", file);
 }
-
