91c9d5eb0d5f1b0a73c3f8edb3d17ef615e6eef2
cy6erlion <cy6erlion@protonmail.com>
2020-01-24 12:43:22 +0200

    added progress bar

--- src/clock.rs
+++ src/clock.rs
@@ -2,12 +2,18 @@ use console::style;
 use console::Term;
 
 use crate::controls::{ctrl_pomo, Ctrl};
+use indicatif::{ProgressBar, ProgressStyle};
 use std::thread;
 use std::time::Duration;
 
 pub fn countdown(count_from: u64, todo_name: String) -> Result<(), ()> {
     let term = Term::stdout();
+    let bar = ProgressBar::new(count_from);
 
+    bar.set_style(ProgressStyle::default_bar()
+                  .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+                  .progress_chars("##-"));
+    
     term.clear_screen();
     let handle = thread::spawn(move || {
         println!(
@@ -15,22 +21,20 @@ pub fn countdown(count_from: u64, todo_name: String) -> Result<(), ()> {
             style("[CLOCK started]").blue().dim(),
             count_from
         );
-        println!(
-            "{}",
-            style("+--------------------------------------------------------+").blue()
-        );
+        
         for i in (1..=count_from).rev() {
-            println!(" {} min remaining...", i);
+            bar.inc(1);  // Increment loader
             thread::sleep(Duration::from_secs(1));
         }
         println!(
             "{}",
             style("+--------------------------------------------------------+").blue()
         );
+                
     });
 
     handle.join().unwrap();
-
+        
     term.clear_screen();
     println!("{} {}min", style("[CLOCK ended]").blue().dim(), count_from);
     println!(
